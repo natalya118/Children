ad2ccecb2194a0e06d181099c01aa6af42c27815
natalya118 <nutblog88@gmail.com>
2017-05-30 03:26:52 +0300

    added some models

---
+++ VS2015/src/A2SPA/Models/Need.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A2SPA.Models
+{
+    public enum Category
+    {
+            Clothes, 
+            Food,
+            Medicine,
+            Other
+        }
+
+    public enum Type
+    {
+        Urgently,
+        Not_Urgently,
+        Wanted
+    }
+    public class Need
+    {
+        
+        public int NeedId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category? Category { get; set; }
+        public Type? Type { get; set; }
+        public int ChildId { get; set; }
+
+        public Child Child { get; set; }
+    }
+}
